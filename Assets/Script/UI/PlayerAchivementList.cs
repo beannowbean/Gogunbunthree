5206d9b7718322ed374fc3b83eb02886e3c6ace7
gamza <lee77347559@gmail.com>
2026-01-21 11:32:38 +0900

    Add Player achivement script
    플레이어가 업적을 달성하였을 때,
    불러오는 로직 script 추가

---
+++ Assets/Script/UI/PlayerAchivementList.cs
@@ -0,0 +1,146 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAchivementList : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /* 
+     * 난이도 1짜리 업적 목록
+    */
+
+    public void Newbie()
+    {
+
+    }
+
+    public void Dumb()
+    {
+
+    }
+
+    /* 
+     * 난이도 2짜리 업적 목록
+    */
+
+    public void HitAndRun()
+    {
+
+    }
+
+    public void TreasureHunter()
+    {
+
+    }
+
+    public void Eagle()
+    {
+
+    }
+
+    /* 
+     * 난이도 3짜리 업적 목록
+    */
+
+    public void Bunny()
+    {
+
+    }
+
+    public void Bruh()
+    {
+
+    }
+
+    public void Acrophobia()
+    {
+
+    }
+
+    /* 
+     * 난이도 4짜리 업적 목록
+    */
+
+    public void TopGun()
+    {
+
+    }
+
+    public void HeliVIP()
+    {
+
+    }
+
+    public void Gentleman()
+    {
+
+    }
+
+    public void Wrecker()
+    {
+
+    }
+
+    /* 
+     * 난이도 5짜리 업적 목록
+    */
+
+    public void Superstar()
+    {
+
+    }
+
+    public void Iceman()
+    {
+
+    }
+
+    public void Icarus()
+    {
+
+    }
+
+    public void Hustler()
+    {
+
+    }
+
+    /* 
+     * 난이도 6짜리 업적 목록
+    */
+
+    public void SkyWalker()
+    {
+
+    }
+
+    public void Pennyless()
+    {
+
+    }
+
+    /* 
+     * 난이도 7짜리 업적 목록
+    */
+
+    public void Billionaire()
+    {
+
+    }
+
+    public void Rapunzel()
+    {
+
+    }
+}
